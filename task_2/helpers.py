a294aaf26dfa6c28b41c76820fb6f246fbcb747b
JohnRitchie <lasarenko91@mail.ru>
2024-07-08 12:09:43 +0300

    helpers

---
+++ task_2/helpers.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+
+class HelperRandom:
+    @classmethod
+    def get_str(cls):
+        return ''.join(random.choices(string.ascii_lowercase, k=5))
+
+    @classmethod
+    def get_full_name(cls):
+        return f'{cls.get_str()} {cls.get_str()}'
+
+    @classmethod
+    def get_email_address(cls):
+        return f'{cls.get_str()}@{cls.get_str()}.com'
